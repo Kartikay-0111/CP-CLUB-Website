9087452aab9a2f5230f7a0e5758b6ca36a63c52b
VedantKale08 <fscocomputer@gmail.com>
2024-11-07 20:03:48 +0530

    small changes

--- app/profile/[username]/page.js
+++ app/profile/[username]/page.js
@@ -11,6 +11,9 @@ async function page({params}) {
   const leetCodeData = await getLeetCodeData(user_data.lc_username);
   const codeForcesData = await getCodeForcesData(user_data.cf_username);
 
+  console.log(leetCodeData);
+  console.log(codeForcesData);
+  
   return (
     <div>
       <Profile />
